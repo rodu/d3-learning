6e6479ae341cea4a99bdffecd2d58b655955d726
Roberto Schioppa <roberto.schioppa@propylon.com>
2014-05-15 12:55:38 +0100

    Adds linear scale

--- lets-make-a-bar-chart/bar-chart-1.js
+++ lets-make-a-bar-chart/bar-chart-1.js
@@ -1,6 +1,11 @@
 /* global: d3 */
 window.onload = function onLoad(){
-    var data = d3.range(10, 400, 20);
+    var
+        MAX = 400,
+        data = d3.range(10, MAX, 20),
+        scaleX = d3.scale.linear()
+                    .domain([0, MAX])
+                    .range([0, MAX]);
     d3.select('.chart')
         .selectAll('div')
         .data(data)
@@ -8,8 +13,8 @@ window.onload = function onLoad(){
         .style('height', 20 + 'px')
         .style('width', function width(d){
             var rnd1 = Math.random() * (5 - 2) + 2;
-                rnd2 = Math.random() * (7 - 1) + 1;
-            return Math.min(d * rnd1 / rnd2, 750) + 'px';
+                rnd2 = Math.random() * (7 - 3) + 3;
+            return scaleX(d * rnd1 / rnd2) + 'px';
         })
         .text(function text(d){
             return d;

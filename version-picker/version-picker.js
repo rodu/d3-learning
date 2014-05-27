da649986b2e1e9ae457052fd577ecda9507fffa7
Roberto Schioppa <roberto.schioppa@propylon.com>
2014-05-27 16:56:29 +0100

    Adds central line

--- version-picker/version-picker.js
+++ version-picker/version-picker.js
@@ -1,6 +1,8 @@
 /* global: d3 */
 window.onload = function onLoad(){
     var
+        RANGE_MIN = new Date("01/01/2004"),
+        RANGE_MAX = new Date("31/12/2014"),
         randomDates = function randomDates(minDate, maxDate, numEntries) {
             var min = minDate.getTime(),
             max = maxDate.getTime(),
@@ -15,13 +17,13 @@ window.onload = function onLoad(){
         };
         
         data = randomDates(
-            new Date("01/01/2014"),
+            new Date("01/01/2004"),
             new Date("12/31/2014"),
-            25
+            Math.random() * 45 + 5
         ),
         
         x = d3.time.scale.utc()
-            .domain([new Date("01-01-2014"), new Date("11-01-2014")])
+            .domain([new Date("01-01-2004"), new Date("12-01-2014")])
             .range([10, 790]),
         
         // Let's add the X axis
@@ -32,7 +34,17 @@ window.onload = function onLoad(){
         versionPicker = d3.select('.version-picker')
             .append("svg")
             .attr("width", "100%")
-            .attr("height", 100)
+            .attr("height", 100);
+
+        versionPicker.append("line")
+            .attr("x1", "0")
+            .attr("y1", "50")
+            .attr("x2", "100%")
+            .attr("y2", "50")
+            .attr("stroke", "#000")
+            .attr("stroke-width", "1");
+        
+        versionPicker
             .selectAll('circle')
             .data(data)
             .enter().append('circle')
@@ -41,7 +53,7 @@ window.onload = function onLoad(){
             })
             .attr("cy", 50)
             .attr("r", 5)
-            .attr("fill", "#DA9090")
+            .attr("fill", "#D6C9C9")
             .attr("stroke", "#980606")
             .append("title")
             .text(function textFn(d){
